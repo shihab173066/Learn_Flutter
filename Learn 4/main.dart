c037819debcca28121da6b722c40c208c3c7a757
Md. Shihab Hossain <67728158+shihab173066@users.noreply.github.com>
2022-01-12 18:23:25 +0600

    Add files via upload

---
+++ Learn 4/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: NinjaCard(),
+));
+
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[200],
+      appBar: AppBar(
+        title: Text('Smart Healthcare UI'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                radius: 40.0,
+                backgroundImage: AssetImage('assets/thumb.jpg'),
+              ),
+            ),
+            Divider(
+              color: Colors.grey[800],
+              height: 60.0,
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Md. Shihab Hossain',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'HOMETOWN',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Dhaka, Bangladesh',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'Emergency Level',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '3',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'shihab.hossainSH@gmail.com',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
